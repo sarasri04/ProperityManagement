106dbca7ad6256f5b014ea97bde590550224d458
sarasri04 <srithar4763@gmail.com>
2023-07-11 00:06:19 +1000

    login commit

---
+++ CodingTest/src/main/java/com/market/simplified/controller/loginController.java
@@ -0,0 +1,19 @@
+package com.market.simplified.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class loginController {
+
+	String appName = "SSLF";
+	@GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("appName", appName);
+        return "login";
+    }
+
+	}
+
